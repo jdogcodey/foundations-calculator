ecb37a60b6ed92aeeb7687c2cb1b59bdf5e7cf39
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-12 21:37:12 +0000

    Added operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let number1 = 0;
+let number2 = 0;
+let operator = "";
+
 //Addition function
 const add = function (input1, input2) {
   return input1 + input2;
@@ -16,4 +20,8 @@ const multiply = function (input1, input2) {
 //Division function
 const divide = function (input1, input2) {
   return input1 / input2;
+};
+
+const operate = function (number1, operator, number2) {
+  return operator(number1, number2);
 };
