c47e969b2413ab5c9f7c6b5d9bc1c6adfe413139
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-12 21:28:38 +0000

    Created functions for operations

---
+++ script.js
@@ -0,0 +1,19 @@
+//Addition function
+const add = function (input1, input2) {
+  return input1 + input2;
+};
+
+//Subtraction function
+const subtract = function (input1, input2) {
+  return input1 - input2;
+};
+
+//Multiplication function
+const multiply = function (input1, input2) {
+  return input1 * input2;
+};
+
+//Division function
+const divide = function (input1, input2) {
+  return input1 / input2;
+};
